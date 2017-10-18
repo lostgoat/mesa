3f080537966ff9e102b30131334c7e7fe7e86aea
Andres Rodriguez <andresx7@gmail.com>
2017-10-18 18:36:34 -0400

    radeonsi: advertise support for GL_EXT_semaphore
    Expose the feature only when we have syncobj support available
    
    Signed-off-by: Andres Rodriguez <andresx7@gmail.com>

--- src/gallium/drivers/radeonsi/si_pipe.c
+++ src/gallium/drivers/radeonsi/si_pipe.c
@@ -573,6 +573,9 @@ static int si_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 			return RADEON_SPARSE_PAGE_SIZE;
 		return 0;
 
+	case PIPE_CAP_SEMAPHORE:
+		return sscreen->b.info.has_syncobj;
+
 	/* Unsupported features. */
 	case PIPE_CAP_BUFFER_SAMPLER_VIEW_RGBA_ONLY:
 	case PIPE_CAP_TGSI_FS_COORD_ORIGIN_LOWER_LEFT:
@@ -589,7 +592,6 @@ static int si_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 	case PIPE_CAP_POLYGON_MODE_FILL_RECTANGLE:
 	case PIPE_CAP_POST_DEPTH_COVERAGE:
 	case PIPE_CAP_TILE_RASTER_ORDER:
-	case PIPE_CAP_SEMAPHORE:
 		return 0;
 
 	case PIPE_CAP_NATIVE_FENCE_FD:
